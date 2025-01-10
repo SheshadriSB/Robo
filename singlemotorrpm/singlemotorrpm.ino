c29878a247aed187f32504bd1e0e5b696b252b49
SheshadriSB <sheshadrishikhar@gmail.com>
2025-01-10 21:22:06 +0545

    testing multiple ides

---
+++ singlemotorrpm/singlemotorrpm.ino
@@ -0,0 +1,35 @@
+volatile long pulseCount = 0;  
+unsigned long lastTime = 0;     
+const int ppr = 210;              
+const int encoderPin = 2;        
+
+//change from arduino
+
+//change from vscode
+
+void countPulses() {
+  pulseCount++; 
+}
+
+void setup() {
+  pinMode(encoderPin, INPUT_PULLUP);                  
+  attachInterrupt(digitalPinToInterrupt(encoderPin), countPulses, RISING); 
+  Serial.begin(9600);                                 
+}
+
+void loop() {
+  unsigned long currentTime = millis();              
+
+  if (currentTime - lastTime >= 1000) {              
+    noInterrupts();                                
+    long pulses = pulseCount;                     
+    pulseCount = 0;                                 
+    interrupts();                                  
+
+    float rpm = (pulses * 60.0) / ppr;              
+    Serial.print("RPM: ");
+    Serial.println(rpm);                            
+
+    lastTime = currentTime;                        
+  }
+}
